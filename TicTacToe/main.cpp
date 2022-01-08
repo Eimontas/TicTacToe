98720612b2901689d2c97e4c45a454b540779689
Eimontas <ejankauskis20@gmail.com>
2022-01-08 16:51:10 +0000

    Added Win/ Loss Options

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -9,18 +9,20 @@
 int main() {
 
 	while (true) {
+		//0.1. Initialise key variables and print init statement.
 		TicTacToe board;
 		int keyValue = 0;
+		char playAgainChoice = 'x';
+		bool invalidMove = false;
 		cout << "Welcome to Tic Tac Toe! " << endl;
 		cout << "Use the arrow keys to select a tile, and Enter to place your move." << endl;
 
 		while (board.getCurState() == inProgress) {
 			system("CLS");
 
-			//1. Init players turn.
+			//1. Init players turn, and refresh the board state.
 			cout << board.getCurPlayer() << "'s turn: " << endl;
 			board.printBoard();
-			bool invalidCmd = false;
 			//2. Listen for arrow inputs and update UI to show selected tile.
 			switch ((keyValue = _getch())) {
 				case(KEY_LEFT):
@@ -41,15 +43,37 @@ int main() {
 				default:
 					break;
 			}
-			if (board.getCurState() == inProgress) {
+			CURSTATE state = board.getCurState();
+			if (state == inProgress) {
 				board.switchPlayer();
 			}
 			else { 
-				system("CLS");
+				switch (state) {
+				case(draw):
+					cout << "It's a draw!" << endl;
+					break;
+				case(xWin):
+					cout << "Player X is the Winner!!!" << endl;
+					break;
+				case(oWin):
+					cout << "Player O is the Winner!!!" << endl;
+					break;
+				default:
+					break;
+				}
+				cout << "Would you like to play again? (Y/N) \n";
+				do {
+					cin >> playAgainChoice;
+				} while (
+					playAgainChoice != 'Y' && playAgainChoice != 'N' &&
+					playAgainChoice != 'y' && playAgainChoice != 'n'
+					);
 			}
+
 		}
-	
-		break;
+		if (playAgainChoice == 'N' || playAgainChoice == 'n') {
+			break;
+		}
 	}
 	return 0;
 }
